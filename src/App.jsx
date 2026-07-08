36e758a69f6b4fb9aa91fa95fb6a21cb9371873a
Saulo Rangel <saulorangel10@hotmail.com>
2026-07-08 08:16:50 -0300

    feat: restructure App component layout and add new components (Container, Main, Searchinput) with icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { Aside } from "./components/Aside";
+import { Container } from "./components/Container";
+import { Main } from "./components/Main";
+import { Searchinput } from "./components/Searchinput";
 
 function App() {
   return (
     <div className="bg-neutral-background min-h-screen">
-      <Aside />
+      <Container>
+        <Aside />
+        <Main>
+          <Searchinput name="q" />
+        </Main>
+      </Container>
     </div>
   );
 }
